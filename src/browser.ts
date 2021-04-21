54b655beea98e0858ae589f2ce9a3587931efeef
Chris Hager <chris@linuxuser.at>
2021-04-21 16:32:59 +0200

    window cast info

--- src/browser.ts
+++ src/browser.ts
@@ -5,6 +5,6 @@
 import { foo } from './main'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).foo = foo
+(window as any).foo = foo  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
 
 console.log('Method "foo" was added to the window object. You can try it yourself by just entering "await foo()"')
